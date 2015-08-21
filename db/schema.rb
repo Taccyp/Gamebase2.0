5fca76aaa818cd7520fd2ae3d66f4949dfe120e0
Patrick Keenan <pgk4526@gmail.com>
2015-08-21 12:20:54 -0400

    moved search bar to the nav bar so it appears on all pages in a convenient spot, other minor css tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818185305) do
+ActiveRecord::Schema.define(version: 20150821160532) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "game_id"
     t.string   "game_name"
     t.string   "platforms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/views/reviews/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "reviews/index", type: :view do
+  before(:each) do
+    assign(:reviews, [
+      Review.create!(
+        :title => "Title",
+        :body => "MyText"
+      ),
+      Review.create!(
+        :title => "Title",
+        :body => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of reviews" do
+    render
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
