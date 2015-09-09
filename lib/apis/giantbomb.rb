b2c06f6468d0f9598a97c3510831cbd13c2d22c4
Patrick Keenan <pgk4526@gmail.com>
2015-09-08 20:45:19 -0400

    hiding api key inside a variable

--- lib/apis/giantbomb.rb
+++ lib/apis/giantbomb.rb
@@ -4,12 +4,12 @@ require 'json'
 class APIS::Giantbomb
 
 def game_search(game_name)
-  hit_api("http://www.giantbomb.com/api/search/?api_key=1a41f94a37f18d50c2145d005aa5c56f0b519232&format=json&query=#{game_name}&resources=game")
+  hit_api("http://www.giantbomb.com/api/search/?api_key=#{ENV['giantbomb_api']}&format=json&query=#{game_name}&resources=game")
 
 end
 
 def game_by_id(id)
-  hit_api("http://www.giantbomb.com/api/game/#{id}/?api_key=1a41f94a37f18d50c2145d005aa5c56f0b519232&format=json")
+  hit_api("http://www.giantbomb.com/api/game/#{id}/?api_key=1#{ENV['giantbomb_api']}&format=json")
 end
 
 def hit_api(inputstring)
