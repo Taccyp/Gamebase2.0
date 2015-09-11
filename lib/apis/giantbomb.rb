f021c7cabd3d43462ecb2eb4e1a52e21b85419b4
Patrick Keenan <pgk4526@gmail.com>
2015-09-11 17:27:23 -0400

    proxy routing

--- lib/apis/giantbomb.rb
+++ lib/apis/giantbomb.rb
@@ -3,19 +3,21 @@ require 'json'
 
 class APIS::Giantbomb
 
-def game_search(game_name)
-  hit_api("http://www.giantbomb.com/api/search/?api_key=#{ENV['giantbomb_api']}&format=json&query=#{game_name}&resources=game")
+  def game_search(game_name)
+    hit_api("http://www.giantbomb.com/api/search/?api_key=#{ENV['giantbomb_api']}&format=json&query=#{game_name}&resources=game")
 
-end
+  end
 
-def game_by_id(id)
-  hit_api("http://www.giantbomb.com/api/game/#{id}/?api_key=1#{ENV['giantbomb_api']}&format=json")
-end
+  def game_by_id(id)
+    hit_api("http://www.giantbomb.com/api/game/#{id}/?api_key=1#{ENV['giantbomb_api']}&format=json")
+  end
 
-def hit_api(inputstring)
-  escapedString = URI::escape(inputstring)
-  uri = URI(escapedString)
-  JSON.parse(Net::HTTP.get(uri))
-end
+  def hit_api(inputstring)
+    proxy = Net::HTTP::Proxy("http://quotaguard3596:a62d6d50d42f@us-east-1-static-hopper.quotaguard.com
+      ",9293)
+    escapedString = URI::escape(inputstring)
+    uri = URI(escapedString)
+    JSON.parse(proxy.get(uri))
+  end
 
 end
