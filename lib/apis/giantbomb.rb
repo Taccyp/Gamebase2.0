4560baa6b46727096b7cbc199a23d5c49cb4d597
Patrick Keenan <pgk4526@gmail.com>
2015-09-20 22:01:29 -0400

    api requests go through my proxy now, should work on heroku

--- lib/apis/giantbomb.rb
+++ lib/apis/giantbomb.rb
@@ -4,23 +4,18 @@ require 'json'
 class APIS::Giantbomb
 
   def game_search(game_name)
-    hit_api("http://www.giantbomb.com/api/search/?api_key=#{ENV['giantbomb_api']}&format=json&query=#{game_name}&resources=game")
+    hit_api("http://192.168.1.100/phproxy/index.php?q=http%3A%2F%2Fwww.giantbomb.com%2Fapi%2Fsearch%2F%3Fapi_key%3D1a41f94a37f18d50c2145d005aa5c56f0b519232%26format%3Djson%26query%3D%#{game_name}%22%26resources%3Dgame&hl=3e5")
 
   end
 
   def game_by_id(id)
-    hit_api("http://www.giantbomb.com/api/game/#{id}/?api_key=1#{ENV['giantbomb_api']}&format=json")
+    hit_api("http://192.168.1.100/phproxy/index.php?q=http%3A%2F%2Fwww.giantbomb.com%2Fapi%2Fgame%2F#{id}%2F%3Fapi_key%3D1a41f94a37f18d50c2145d005aa5c56f0b519232%26format%3Djson&hl=3e5")
   end
 
-  def hit_api(inputstring)
-    proxyurl = "us-east-1-static-hopper.quotaguard.com"
-    proxyport = 9293
-    user = "quotaguard3596"
-    pass = "a62d6d50d42f"
-    proxy = Net::HTTP::Proxy(proxyurl,proxyport,user,pass)
-    escapedString = URI::escape(inputstring)
-    uri = URI(escapedString)
-    JSON.parse(proxy.get(uri))
-  end
+def hit_api(inputstring)
+  escapedString = URI::escape(inputstring)
+  uri = URI(escapedString)
+  JSON.parse(Net::HTTP.get(uri))
+end
 
 end
